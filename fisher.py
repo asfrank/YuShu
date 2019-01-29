59b457764de3b44cb9e245fbf9c8226cceee2246
asfrank <asfrank@qq.com>
2019-01-29 17:00:32 +0800

    数据库模型类book

--- fisher.py
+++ fisher.py
@@ -1,19 +1,6 @@
-from flask import Flask, jsonify
-from helper import is_isbn_or_key
-from yushu_book import YuShuBook
+from app import create_app
 
-app = Flask(__name__)
-app.config.from_object('config')
-
-@app.route('/book/search/<q>/<page>')
-def search(q,page):
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
-    else:
-        result = YuShuBook.search_by_keyword(q)
-    return jsonify(result)
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=app.config.get('DEBUG'))
