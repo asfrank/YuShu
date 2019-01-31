c3f9af2a3e82af447146e89f9148687b610faf3f
asfrank <asfrank@qq.com>
2019-01-31 10:33:48 +0800

    加入静态文件，模板文件

--- app/web/book.py
+++ app/web/book.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import jsonify,request
+from flask import jsonify, request, render_template, flash
 from app.forms.book import SearchForm
 
 from app.libs.helper import is_isbn_or_key
@@ -11,16 +11,22 @@ from . import web
 @web.route('/book/search')
 def search():
     form = SearchForm(request.args)
+    books = BookCollection()
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
-        books = BookCollection()
         yushubook = YuShuBook()
         if isbn_or_key == 'isbn':
             yushubook.search_by_isbn(q)
         else:
             yushubook.search_by_keyword(q,page)
         books.fill(yushubook, q)
-        return json.dumps(books, default=lambda x:x.__dict__)
-    return jsonify(form.errors)
+        # return json.dumps(books, default=lambda x:x.__dict__)
+    else:
+        flash('搜索的关键字不符合要求，请重新输入关键字')
+    return render_template('search_result.html',books = books)
+
+@web.route('/book/<isbn>/detail')
+def book_detail(isbn):
+    pass
